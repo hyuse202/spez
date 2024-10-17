833b9993ca9c9a540b39be9ccaa2af12843738c9
hyuse202 <buinguyentanhuy2002@gmail.com>
2024-10-17 15:31:56 +0700

    fix(be/cors): add origins domains

--- spez-backend/app/main.py
+++ spez-backend/app/main.py
@@ -1,6 +1,7 @@
 # app/main.py
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import engine, Base
 from .router import route
 
@@ -12,6 +13,19 @@ app = FastAPI(
     description="SPez app",
     version="0.2.0",
 )
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000"
+    # Add your frontend domain here
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Include the items router
 app.include_router(route.router)
